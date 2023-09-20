9e21514fd2e43c20ba55d6c0b458f2424b906e86
Shakiful <shakifulkhan@gmail.com>
2023-09-20 15:30:08 +0600

    added the time of UTC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TimeApiService } from './time-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setInterval(() => {
+      this.getTime();
+    }, 1000);
+
+    this.getTime();
+  }
+  constructor(private timeApi: TimeApiService) {}
+
   title = 'timezone';
+  timezone: string = 'BST';
+  time: string = '';
+
+  getTime(): any {
+    this.timeApi.getCurrentTime(this.timezone).subscribe((date) => {
+      console.log(this.timezone, 'here');
+
+      this.time = date.current_time;
+      return date.current_time;
+    });
+  }
 }
--- src/app/time-api.service.ts
+++ src/app/time-api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TimeApiService {
+  private apiUrl = 'http://localhost/clock_app/Backend/timezone_api.php';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getCurrentTime(timezone: string): Observable<any> {
+    const url = `${this.apiUrl}?timezone = ${timezone}`;
+    return this.http.get(url);
+  }
 }
