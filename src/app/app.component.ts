0f7af5e8af7660dd870f4c17973f179c6705a5f4
Shakiful <shakifulkhan@gmail.com>
2023-09-24 14:46:34 +0600

    design full done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TimeApiService } from './time-api.service';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -21,9 +22,33 @@ export class AppComponent implements OnInit {
   time: string = '';
   digits: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
+  hourHandPosition = 0;
+  minuteHandPosition = 0;
+  secondHandPosition = 0;
+  meridiem = '';
+
   getTime() {
     this.timeApi.getCurrentTime(this.timezone).subscribe((date) => {
       this.time = date.current_time;
+
+      const [hours, minutes, seconds, meridiem] = this.time.split(':');
+
+      this.meridiem = meridiem;
+
+      // Assign the extracted values to your component properties
+      this.secondHandPosition = Number(seconds);
+      this.minuteHandPosition = Number(minutes);
+      this.hourHandPosition = Number(hours);
+      //positioning the hands
+      this.secondHandPosition = this.secondHandPosition * 6;
+      this.minuteHandPosition = this.minuteHandPosition * 6;
+      this.hourHandPosition =
+        (this.hourHandPosition > 11
+          ? this.hourHandPosition - 12
+          : this.hourHandPosition) *
+          30 +
+        Math.floor(Number(minutes) / 12) * 6;
+
       return date.current_time;
     });
   }
